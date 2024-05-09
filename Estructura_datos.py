4a5b8a5e12f9d361bf982d0fbd3b0fa2a1fadcf9
NicolasGuerrero <nicoguerrero0800@gmail.com>
2024-05-08 22:10:35 -0600

    Retos de logica de programacion

---
+++ Estructura_datos.py
@@ -0,0 +1,14 @@
+# ESTRUCTURA DE DATOS
+
+# listas
+my_list = ["nico", "marce", "mari", "agus", "gaby"]
+print(my_list)
+my_list.append("sam") # inserccion
+print(my_list)
+my_list.remove("mari") # eliminacion
+print(my_list)
+print(my_list[1]) # acceso
+my_list[1] = "nacho"
+print(my_list)
+my_list.sort() # ordenacion
+print(my_list)
