ec0671a5106d1071af5314c26406a0c6f1c44e45
NicoGuerrero11 <nicoguerrero0800@gmail.com>
2024-08-31 13:49:25 -0600

    Herencia

--- JS/09 - HERENCIA/index.js
+++ JS/09 - HERENCIA/index.js
@@ -43,34 +43,75 @@ gato.emitirSonido();
 // --- extra ---- // 
 
 class Empleado {
-    constructor(nombre, identificador){
+    constructor(nombre, id){
         this.nombre = nombre;
-        this.identificador = identificador;
+        this.id = id;
+        this.empleados = [];
+    }
+    add(empleado){
+        this.empleados.push(empleado);
+    }
+
+    mostrarEmpleado(){
+        for(let empleado of this.empleados){
+            console.log(empleado);
+        }
     }
 }
 
 class Gerente extends Empleado{
-    constructor(nombre, identificador){
-        super(nombre);
-        super(identificador);
+    coordinar_proyectos(){
+        console.log(`${this.nombre} esta coordinando proyectos`);
     }
 }
 
 class GerenteProyecto extends Empleado {
-    constructor(nombre, identificador){
-        super(nombre);
-        super(identificador);
+    constructor(nombre, id, proyecto){
+        super(nombre, id);
+        this.proyecto = proyecto;
+    }
+
+    coordinar_proyecto(){
+        console.log(`${this.nombre} esta coordinando un proyecto`);
     }
 }
 
 class Programador extends Empleado{
-    constructor(nombre, identificador){
-        super(nombre);
-        super(identificador);
+    constructor(nombre, id,lenguaje){
+        super(nombre, id);
+        this.lenguaje = lenguaje;
+    }
+
+    code(){
+        console.log(`${this.nombre} esta programando en el lenguje: ${this.lenguaje}`);
+    }
+
+    add(employee = Empleado){
+        console.log(`un programador no tiene empleados, ${employee.nombre} no se agregara`);
     }
 }
 
+my_manager = new Gerente("Nico", 1);
+my_proyect_manager = new GerenteProyecto("Luis", 2, "proyecto 1");
+my_proyect_manager2 = new GerenteProyecto("Marco", 3, "proyecto 2");
+my_programmer = new Programador("seba", 4, "python");
+my_programmer2 = new Programador("Javier", 5, "JavaScript");
+my_programmer3 = new Programador("Marce", 6, "C++");
+my_programmer4 = new Programador("Jose", 7, "Java");
 
-const gerentePrincipal = new Gerente("Marcos", "gerente");
-const gerenteSegundo = new GerenteProyecto("Luis", "gerente de proyecto");
-const programador = new Programador("nico", "programador");
+my_manager.add(my_proyect_manager);
+my_manager.add(my_proyect_manager2);
+
+my_proyect_manager.add(my_programmer);
+my_proyect_manager.add(my_programmer2);
+my_proyect_manager2.add(my_programmer3);
+my_proyect_manager2.add(my_programmer4);
+
+my_programmer.add(my_programmer2);
+
+my_programmer.code();
+my_proyect_manager.coordinar_proyecto();
+my_manager.coordinar_proyectos();
+
+my_manager.mostrarEmpleado();
+my_proyect_manager.mostrarEmpleado();
